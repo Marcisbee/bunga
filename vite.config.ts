70d93d8e4286b9fc807edb72211d90174977b142
Marcis Bergmanis <marcisbergmanis@gmail.com>
2021-11-27 13:03:07 +0200

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -1,20 +1,30 @@
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
-import { visualizer } from 'rollup-plugin-visualizer';
+// import { visualizer } from 'rollup-plugin-visualizer';
 
 // https://vitejs.dev/config/
-export default defineConfig({
-  plugins: [
-    react(),
-    visualizer(),
-  ],
-  build: {
-    minify: 'esbuild',
-  },
-  resolve: {
-    alias: {
-      react: 'preact/compat',
-      'react-dom': 'preact/compat',
+export default defineConfig(({ command }) => {
+  if (command === 'serve') {
+    return {
+      plugins: [
+        react(),
+      ],
+    };
+  }
+
+  return {
+    plugins: [
+      react(),
+      // visualizer(),
+    ],
+    build: {
+      minify: 'esbuild',
     },
-  },
+    resolve: {
+      alias: {
+        react: 'preact/compat',
+        'react-dom': 'preact/compat',
+      },
+    },
+  };
 })
