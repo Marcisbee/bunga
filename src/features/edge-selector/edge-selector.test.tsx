0458652f9dd89e278c3ad4425258c037e53c36c0
Marcis Bergmanis <marcisbergmanis@gmail.com>
2021-12-07 19:35:45 +0200

    fix tests

--- src/features/edge-selector/edge-selector.test.tsx
+++ src/features/edge-selector/edge-selector.test.tsx
@@ -15,15 +15,27 @@ test('returns function', () => {
   assert.type(EdgeSelectorComponent, 'function');
 });
 
-test('renders component correctly', () => {
+test('renders inactive component correctly', () => {
   const { container } = render((
     <EdgeSelectorComponent />
   ));
 
   assert.snapshot(
     container.innerHTML,
-    '<div>EdgeSelector</div>',
+    '<div><div><button type="button" class=""><svg xmlns="http://www.w3.org/2000/svg" width="14" height="14" viewBox="0 0 24 24"><path d="M24 10h-10v-10h-4v10h-10v4h10v10h4v-10h10z"></path></svg></button></div></div>',
   );
+});
+
+test('renders input when button clicked', () => {
+  const { container } = render((
+    <EdgeSelectorComponent />
+  ));
+
+  container.querySelector('button')!.click();
+
+  const input = container.querySelector('input')!;
+
+  assert.ok(input);
 });
 
 test.run();
