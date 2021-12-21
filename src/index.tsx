b8c7f446dc351fc71fbb20c9d51261ed45dfef91
Marcis Bergmanis <marcisbergmanis@gmail.com>
2021-12-21 12:42:12 +0200

    upgrade to react 18

--- src/index.tsx
+++ src/index.tsx
@@ -1,13 +1,14 @@
 import * as React from 'react';
-import { render } from 'react-dom';
+import { render, createRoot } from 'react-dom';
 
 import { Router } from './pages/router';
 
 import './styles/index.scss';
 
-render(
+const root = createRoot(document.getElementById('root'));
+
+root.render(
   <React.StrictMode>
     <Router />
   </React.StrictMode>,
-  document.getElementById('root'),
 );
